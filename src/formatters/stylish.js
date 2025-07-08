ed36c6ff3230f22d190ae912f3e55c37dc690434
lokalin <ilya-1974@yandex.ru>
2025-07-08 12:32:32 +0300

    t: render tree

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,56 +1,26 @@
-// @ts-check
-import _ from 'lodash';
+const indent = (depth, spacesCount = 4) => ' '.repeat(depth * spacesCount - 2)
 
-const INDENT = 4;
-const INDENT_OFFSET = 2;
+const mapping = {
+  root: ({ children }, depth, iter) => {
+    const output = children.flatMap(node => mapping[node.type](node, depth + 1, iter))
+    return `{\n${output.join('\n')}\n}`
+  },
+  added: (node, depth) => `${indent(depth)}+ ${node.key}: ${node.value}`,
+  deleted: (node, depth) => `${indent(depth)}- ${node.key}: ${node.value}`,
+  unchanged: (node, depth) => `${indent(depth)}  ${node.key}: ${node.value}`,
+  changed: (node, depth) => {
+    const { key, value1, value2 } = node
 
-const getIndent = (depth) => {
-  if (depth < 1) return '';
-  return ' '.repeat(depth * INDENT - INDENT_OFFSET);
-};
+    const data1 = `${indent(depth)}- ${key}: ${value1}`
+    const data2 = `${indent(depth)}+ ${key}: ${value2}`
 
-const stringify = (data, depth, render) => {
-  if (!_.isObject(data)) {
-    return String(data);
-  }
+    return [data1, data2]
+  },
+}
 
-  const output = Object
-    .entries(data)
-    .map(([key, value]) => render({ key, value, type: 'unchanged' }, depth + 1));
-  return `{\n${output.join('\n')}\n${getIndent(depth)}  }`;
-};
+const renderTree = (ast) => {
+  const iter = (node, depth) => mapping[node.type](node, depth, iter)
+  return iter(ast, 0)
+}
 
-const render = (node, depth) => {
-  const { key, type } = node;
-  const indent = getIndent(depth);
-
-  switch (type) {
-    case 'root': {
-      const { children } = node;
-      // eslint-disable-next-line no-shadow
-      const output = children.map((node) => render(node, depth + 1));
-      return `{\n${output.join('\n')}\n}`;
-    }
-    case 'nested': {
-      const { children } = node;
-      // eslint-disable-next-line no-shadow
-      const output = children.flatMap((node) => render(node, depth + 1));
-      return `${indent}  ${key}: {\n${output.join('\n')}\n${indent}  }`;
-    }
-    case 'changed': {
-      const data1 = `${indent}- ${key}: ${stringify(node.value1, depth, render)}`;
-      const data2 = `${indent}+ ${key}: ${stringify(node.value2, depth, render)}`;
-      return [data1, data2];
-    }
-    case 'removed':
-      return `${indent}- ${key}: ${stringify(node.value, depth, render)}`;
-    case 'added':
-      return `${indent}+ ${key}: ${stringify(node.value, depth, render)}`;
-    case 'unchanged':
-      return `${indent}  ${key}: ${stringify(node.value, depth, render)}`;
-    default:
-      throw new Error(`Unknown type ${type}`);
-  }
-};
-
-export default (tree) => render(tree, 0);
+export default renderTree
