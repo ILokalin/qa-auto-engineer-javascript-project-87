4ea72774b5a5c992ed198447fea42603cb3fe2c9
lokalin <ilya-1974@yandex.ru>
2025-07-08 23:49:36 +0300

    feat: remove semi colon, add anarchy in code

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
-import genDiff from '../src/index.js';
+import { Command } from 'commander'
+import genDiff from '../src/index.js'
 
-const program = new Command();
+const program = new Command()
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2, program.opts().format));
+    console.log(genDiff(filepath1, filepath2, program.opts().format))
   })
-  .parse(process.argv);
+  .parse(process.argv)
--- src/parsers.js
+++ src/parsers.js
@@ -1,16 +1,16 @@
 // @ts-check
-import yaml from 'js-yaml';
+import yaml from 'js-yaml'
 
 export default (data, format) => {
   switch (format) {
     case 'yaml':
     case 'yml':
-      return yaml.load(data);
+      return yaml.load(data)
 
     case 'json':
-      return JSON.parse(data);
+      return JSON.parse(data)
 
     default:
-      throw new Error(`Unknown format: ${format}`);
+      throw new Error(`Unknown format: ${format}`)
   }
-};
+}
